a757e7667b196c3a6c638731235b2f3a091d2bff
Faris Abdelrahem <farishabdelrahem@gmail.com>
2025-05-20 09:39:57 -0400

    update to about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,12 @@
 function About() {
     return (
         <div>
-        <h1>About</h1>
-        <p>This is the about page.</p>
+            <h1 className="text-[#1B998B] py-5 text-center text-5xl md:text-7xl">about me</h1>
+            <div>
+                <p>i'm faris abdelrahem, a sophomore comptuer science student studying at 
+                    the university of central florida
+                </p>
+            </div>
         </div>
     );
 }
